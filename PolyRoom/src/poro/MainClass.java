fc5f1e7a3287af3bf78a8fab3c1a81d6bcc32195
vinh <vinh@Neko>
2020-11-29 03:03:02 +0700

    Xong cơ bản Update tài khoản

--- PolyRoom/src/poro/MainClass.java
+++ PolyRoom/src/poro/MainClass.java
@@ -18,9 +18,10 @@ public class MainClass {
     public static void main(String[] args) {
         TaiKhoan taiKhoan;
         taiKhoan = new TaiKhoan("test3", "email3", "pass", false, "viz", "0123");
-        
         DatabaseManager dm = new DatabaseManager();
-        
+        taiKhoan.setEmail("new 33333");
+        dm.update(taiKhoan);
+                
         dm.insert(taiKhoan);
         ArrayList<TaiKhoan> al = dm.select(taiKhoan);
         al.forEach(d -> {
