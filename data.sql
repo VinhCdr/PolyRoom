5731b47cc70774788c717dbb0fa99a2cc58a460e
vinh <vinh@Neko>
2020-12-10 20:36:36 +0700

    Fix dữ liệu temp trong csdl

--- data.sql
+++ data.sql
@@ -1,80 +1,82 @@
-USE master;
-GO
-DROP DATABASE polyroom;
-GO
+IF (db_id('polyroom') IS NOT NULL)
+BEGIN
+    USE master;
+    DROP DATABASE polyroom;
+END;
+
 CREATE DATABASE polyroom;
 GO
 USE polyroom;
 GO
 
-CREATE TABLE phong
+CREATE TABLE [phong]
 (
-    id_phong integer NOT NULL,
-    so_tang integer NOT NULL,
-    ten_phong NVARCHAR(32) NOT NULL UNIQUE,
-    is_cho_muon bit NOT NULL,
-    PRIMARY KEY (id_phong, so_tang)
+    [id_phong] integer NOT NULL,
+    [so_tang] integer NOT NULL,
+    [ten_phong] NVARCHAR(32) NOT NULL UNIQUE,
+    [is_cho_muon] bit NOT NULL,
+    PRIMARY KEY ([id_phong], [so_tang])
 );
 GO
 
-CREATE TABLE tai_khoan
+CREATE TABLE [tai_khoan]
 (
-    id_tai_khoan varchar(64) NOT NULL,
-    email varchar(128) NOT NULL,
-    mat_khau varchar(128) NOT NULL,
-    is_phan_quyen bit NOT NULL,
-    ten NVARCHAR(64) NOT NULL,
-    sdt char(10) NOT NULL,
-    PRIMARY KEY (id_tai_khoan)
+    [id_tai_khoan] varchar(64) NOT NULL,
+    [email] varchar(128) NOT NULL,
+    [mat_khau] varchar(128) NOT NULL,
+    [is_phan_quyen] bit NOT NULL,
+    [ten] NVARCHAR(64) NOT NULL,
+    [sdt] char(10) NOT NULL,
+    PRIMARY KEY ([id_tai_khoan])
 );
 GO
 
-CREATE TABLE muon_phong
+CREATE TABLE [muon_phong]
 (
-    id_muon_phong integer NOT NULL IDENTITY(1,1),
-    id_tai_khoan varchar(64) REFERENCES tai_khoan(id_tai_khoan) NOT NULL ,
-    id_phong integer NOT NULL,
-    ly_do NVARCHAR(1024) NOT NULL,
-    so_tang integer NOT NULL,
-    tg_muon datetime NOT NULL,
-    tg_tra datetime NOT NULL,
-    tg_tra_thuc_te datetime NULL,
-    PRIMARY KEY (id_muon_phong),
-    FOREIGN KEY (id_phong, so_tang) REFERENCES phong(id_phong, so_tang)
+    [id_muon_phong] integer NOT NULL IDENTITY(1,1),
+    [id_tai_khoan] varchar(64) REFERENCES [tai_khoan]([id_tai_khoan]) NOT NULL ,
+    [id_phong] integer NOT NULL,
+    [ly_do] NVARCHAR(1024) NOT NULL,
+    [so_tang] integer NOT NULL,
+    [tg_muon] datetime NOT NULL,
+    [tg_tra] datetime NOT NULL,
+    [tg_tra_thuc_te] datetime NULL,
+    PRIMARY KEY ([id_muon_phong]),
+    FOREIGN KEY ([id_phong], [so_tang]) REFERENCES phong([id_phong], [so_tang])
 );
 GO
 
-CREATE TABLE thong_tin_sinh_vien
+CREATE TABLE [thong_tin_sinh_vien]
 (
-    id_sinh_vien varchar(8) NOT NULL,
-    email varchar(128) NOT NULL,
-    ten_sinh_vien NVARCHAR(128) NOT NULL,
-    id_muon_phong integer NOT NULL REFERENCES muon_phong(id_muon_phong),
-    PRIMARY KEY (id_sinh_vien, id_muon_phong)
+    [id_sinh_vien] varchar(8) NOT NULL,
+    [email] varchar(128) NOT NULL,
+    [ten_sinh_vien] NVARCHAR(128) NOT NULL,
+    [id_muon_phong] integer NOT NULL REFERENCES [muon_phong]([id_muon_phong]),
+    PRIMARY KEY ([id_sinh_vien], [id_muon_phong])
 );
 GO
 
-CREATE TABLE temp_muon_phong_sv
+CREATE TABLE [temp_muon_phong_sv]
 (
-    id_temp INTEGER IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    id_phong INTEGER NOT NULL,
-    so_tang INTEGER NOT NULL,
-    tg_muon DATETIME NOT NULL,
-    tg_tra DATETIME NOT NULL,
-    id_sinh_vien VARCHAR(8) NOT NULL,
-    email_sinh_vien VARCHAR(128) NOT NULL,
-    ten_sinh_vien NVARCHAR(128),
-    ly_do NVARCHAR(256),
-    id_tai_khoan VARCHAR(64),
-    otp char(8) NOT NULL,
-    tg_dang_ky DATETIME NOT NULL,
-    FOREIGN KEY (id_phong, so_tang) REFERENCES phong(id_phong, so_tang),
-    FOREIGN KEY (id_tai_khoan) REFERENCES tai_khoan(id_tai_khoan)
+    [id_temp] INTEGER IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    [id_phong] INTEGER NOT NULL,
+    [so_tang] INTEGER NOT NULL,
+    [tg_muon] DATETIME NOT NULL,
+    [tg_tra] DATETIME NOT NULL,
+    [id_sinh_vien] VARCHAR(8) NOT NULL,
+    [email_sinh_vien] VARCHAR(128) NOT NULL,
+    [ten_sinh_vien] NVARCHAR(128),
+    [ly_do] NVARCHAR(256),
+    [id_tai_khoan] VARCHAR(64),
+    [otp] char(8) NOT NULL,
+    [tg_dang_ky] DATETIME NOT NULL,
+    FOREIGN KEY ([id_phong], [so_tang]) REFERENCES phong([id_phong], [so_tang]),
+    FOREIGN KEY ([id_tai_khoan]) REFERENCES tai_khoan([id_tai_khoan])
 );
 GO
 
-INSERT INTO phong
-    (so_tang, id_phong, ten_phong, is_cho_muon)
+INSERT INTO [phong]
+    ([so_tang], [id_phong], [ten_phong], [is_cho_muon])
 VALUES
     (0, 1, N'Đào tạo', 0),
     (0, 2, N'Đảm bảo', 0),
@@ -88,11 +90,11 @@ VALUES
     (3, 2, N'302', 1);
 GO
 SELECT *
-FROM phong;
+FROM [phong];
 
 -- mat_khau equals id_tai_khoan
-INSERT INTO tai_khoan
-    (id_tai_khoan, email, mat_khau, is_phan_quyen, ten, sdt)
+INSERT INTO [tai_khoan]
+    ([id_tai_khoan], [email], [mat_khau], [is_phan_quyen], [ten], [sdt])
 VALUES
     ('vinhlm', 'vinhlmpc01238@fpt.edu.vn', '78f7002p638e8vkpkodcn3krml', 1, N'Lê Minh Vinh', '0856538112'),
     ('ngocnty', 'ngocntypc01187@fpt.edu.vn', '51qftrbvk5g6lrqmll0os82qqo', 1, N'Nguyễn Thị Yến Ngọc', '0123456789'),
@@ -106,10 +108,10 @@ VALUES
     ('tk6', 'tk6@fpt.edu.vn', 'ks29dn6lhdemnurnf3odrhbko', 0, N'Tài khoản 6', '0123456796');
 GO
 SELECT *
-FROM tai_khoan;
+FROM [tai_khoan];
 
-INSERT INTO muon_phong
-    (/*id_muon_phong,*/id_tai_khoan, so_tang, id_phong, tg_muon, tg_tra, tg_tra_thuc_te, ly_do)
+INSERT INTO [muon_phong]
+    (/*id_muon_phong,*/[id_tai_khoan], [so_tang], [id_phong], [tg_muon], [tg_tra], [tg_tra_thuc_te], [ly_do])
 VALUES
     ('vinhlm', 1, 1, '2020-12-22 09:30:00', '2020-12-22 11:30:00', '2020-12-22 11:45:00', N'Dạy học'),
     ('ngocnty', 1, 2, '2020-12-22 09:30:00', '2020-12-22 11:30:00', '2020-12-22 11:45:00', N'Dạy học'),
@@ -123,26 +125,26 @@ VALUES
     ('tk6', 3, 1, '2020-12-22 09:30:00', '2020-12-22 11:30:00', null, N'Dạy học');
 GO
 SELECT *
-FROM muon_phong;
+FROM [muon_phong];
 
-INSERT INTO thong_tin_sinh_vien
-    (id_sinh_vien,email, ten_sinh_vien, ly_do, id_muon_phong)
+INSERT INTO [thong_tin_sinh_vien]
+    ([id_sinh_vien],[email], [ten_sinh_vien], [id_muon_phong])
 VALUES
     ('pc01238', 'vinhlmpc01238@fpt.edu.vn', N'Vinh', 1),
     ('pc01187', 'ngocntypc01187@fpt.edu.vn', N'Ngọc', 2),
-    ('pc01238', 'vinhlmpc01238@fpt.edu.vn', N'Vinh',  3),
+    ('pc01238', 'vinhlmpc01238@fpt.edu.vn', N'Vinh', 3),
     ('pc01187', 'ngocntypc01187@fpt.edu.vn', N'Ngọc', 3),
     ('pc01187', 'ngocntypc01187@fpt.edu.vn', N'Ngọc', 4);
 GO
 SELECT *
-FROM thong_tin_sinh_vien;
+FROM [thong_tin_sinh_vien];
 
-INSERT INTO temp_muon_phong_sv
-    (/*id_temp,*/so_tang, id_phong, tg_muon, tg_tra, id_sinh_vien, email_sinh_vien, ten_sinh_vien, ly_do, id_tai_khoan, otp, tg_dang_ky)
+INSERT INTO [temp_muon_phong_sv]
+    (/*id_temp,*/[so_tang], [id_phong], [tg_muon], [tg_tra], [id_sinh_vien], [email_sinh_vien], [ten_sinh_vien], [ly_do], [id_tai_khoan], [otp], [tg_dang_ky])
 VALUES
     (1, 1, '2020-12-23 09:30:00', '2020-12-23 11:30:00', 'pc01275', 'vinhlmpc01238@fpt.edu.vn', N'Phong Trần', N'Học mỹ thuật', 'tk6', '11223344', '2020-12-22 11:30:00'),
     (1, 2, '2020-12-23 13:00:00', '2020-12-23 15:00:00', 'pc01275', 'vinhlmpc01238@fpt.edu.vn', N'Phong Trần', N'Sinh hoạt câu lạc bộ', 'vinhlm', '12345678', '2020-12-23 09:30:00'),
     (1, 3, '2020-12-23 09:30:00', '2020-12-23 11:30:00', 'pc01275', 'vinhlmpc01238@fpt.edu.vn', N'Phong Trần', N'Học thanh nhạc', 'tk6', '11223344', '2020-12-22 11:30:00');
 GO
 SELECT *
-FROM temp_muon_phong_sv;
+FROM [temp_muon_phong_sv];
