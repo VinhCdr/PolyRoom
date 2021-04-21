af856ceed7309971dc9e711b9912295479ba2144
vohongnhatminh <82791068+vohongnhatminh@users.noreply.github.com>
2021-04-21 16:11:32 +0700

    cm

--- src/test/java/poro/module/db/data/TaiKhoanTest.java
+++ src/test/java/poro/module/db/data/TaiKhoanTest.java
@@ -12,24 +12,35 @@ public class TaiKhoanTest {
 	@Test
 	public void selectAllTest() {
 		TaiKhoan tk = new TaiKhoan();	
-		tk.setEmail("vinhlmpc01238@fpt.edu.vn");
+//		tk.setEmail("vinhlmpc01238@fpt.edu.vn");
+		List<TaiKhoan> tkList = DatabaseManager.executeQuery(tk, TaiKhoan.EXECUTE_SELECT_ALL);
+		assertFalse(tkList.isEmpty());
+	}
+	
+	@Test
+	public void selectUsernameTest() {
+		TaiKhoan tk = new TaiKhoan();	
+		tk.setIdTaiKhoan("vinhlm");
+		List<TaiKhoan> tkList = DatabaseManager.executeQuery(tk, TaiKhoan.EXECUTE_SELECT_BY_ID);
+		assertFalse(tkList.isEmpty());
+	}
+	
+	@Test
+	public void selectEmailTest() {
+		TaiKhoan tk = new TaiKhoan();	
+		tk.setIdTaiKhoan("vinhlm");
 		List<TaiKhoan> tkList = DatabaseManager.executeQuery(tk, TaiKhoan.EXECUTE_SELECT_BY_MAIL);
 		assertFalse(tkList.isEmpty());
 	}
 	
 	@Test
-	public void updateAllTest() {
-		TaiKhoan tk = new TaiKhoan();
-		tk.setIdTaiKhoan("xa43sdf");
-		tk.setEmail("mai43eeel@gm.g");
-		tk.setPhanQuyen(true);
-		tk.setSdt("01238");
-		tk.setTen("name");
-		tk.setMatKhau("xxx");
-		int i = DatabaseManager.executeUpdate(tk, TaiKhoan.EXECUTE_INSERT);
-		
-		assertEquals(i, 1);
-		DatabaseManager.executeUpdate(tk, TaiKhoan.EXECUTE_DELETE_BY_ID);	
+	public void selectUsernameEmailPassTest() {
+		TaiKhoan tk = new TaiKhoan();	
+		tk.setIdTaiKhoan("vinhlm");
+		tk.setEmail("vinhlmpc01238@fpt.edu.vn");
+		tk.setMatKhau("1");
+		List<TaiKhoan> tkList = DatabaseManager.executeQuery(tk, TaiKhoan.EXECUTE_SELECT_BY_USER_OR_EMAIL_AND_PASS);
+		assertFalse(tkList.isEmpty());
 	}
 	
 }
