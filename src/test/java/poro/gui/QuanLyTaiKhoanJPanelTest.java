bfd30c9d899cd5e096d0adf26a2ef8179c9a3cb8
luonghuuloi <82566545+luonghuuloi@users.noreply.github.com>
2021-04-27 00:20:55 +0700

    Test quản lý tài khoản p1

---
+++ src/test/java/poro/gui/QuanLyTaiKhoanJPanelTest.java
@@ -0,0 +1,94 @@
+package poro.gui;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import poro.MainClass;
+import poro.module.Session;
+import test.module.Babysitter;
+import test.module.BigBug;
+
+public class QuanLyTaiKhoanJPanelTest {
+	
+	QuanLyTaiKhoanJPanel qltk;
+	
+	@BeforeClass
+	public void init() throws InterruptedException {
+		Babysitter.showMain();
+		// Đăng nhập
+		BigBug.writeString(MainClass.dangNhapJDialog.txtTaiKhoan, "loilh");
+		BigBug.writeString(MainClass.dangNhapJDialog.txtPassword, "loipass");
+		Thread.sleep(500);
+		MainClass.dangNhapJDialog.btnDangNhap.doClick();
+	}
+
+	@AfterClass
+	public void close() {
+		Babysitter.closeMain();
+	}
+	
+	@BeforeMethod
+	public void resetForm() throws InterruptedException {
+		MainClass.mainJFrame.setGiaoDien(Session.USER.isPhanQuyen());
+		Thread.sleep(500);
+		MainClass.mainJFrame.mniQLTaiKhoanActionPerformed(null);
+		qltk = Babysitter.getTabPanelSelected();
+		Thread.sleep(200);
+	}
+
+	@AfterMethod
+	public void delay() throws InterruptedException {
+		Thread.sleep(500);
+	}
+	
+  @Test(timeOut = 10000, priority = 0, dataProvider = "InsertAccount")
+  public void dangKyTaiKhoan(String username, String email, String fname, String phone, String password, String confirmPass) throws InterruptedException {
+//	  qltk = Babysitter.getWindow(QuanLyTaiKhoanJPanel.class);  dòng này bị lỗi???
+	  
+	  BigBug.writeString(qltk.txtTenTaiKhoan, username);
+	  BigBug.writeString(qltk.txtEmail, email);
+	  BigBug.writeString(qltk.txtHoVaTen, fname);
+	  BigBug.writeString(qltk.txtSoDienThoai, phone);
+	  BigBug.writeString(qltk.txtMatKhau, password);
+	  BigBug.writeString(qltk.txtXacNhanMatKhau, confirmPass);
+	  //Còn phần kiểm tra checkbox???
+	  
+	  Thread.sleep(200);
+		new Thread(() -> {
+			qltk.btnThem.doClick();
+		}).start();
+		Thread.sleep(500);
+
+		
+//		JPanel jp = Babysitter.getWindow(jp, JDialog.class);
+		
+		Thread.sleep(200);
+		
+//		Assert.assertEquals(jp.getTitle(), "Message");  dòng này bị lỗi???
+		
+//		jp.setVisible(false);
+		Thread.sleep(200);
+		
+		
+		Assert.assertFalse(MainClass.mainJFrame.isVisible());
+  }
+  
+  @DataProvider(name = "InsertAccount")
+	public Object[][] getAccount() {
+		return new Object[][] {
+				{"loilhpc01261", "loilh@gmail.com", "Lương Hữu Lợi", "0909123456", "123abc", "123abc"},
+				{"loilh123", "loilh@gmail.com", "", "", "", ""},
+				{"loilh123", "emailsai", "Lương Hữu Lợi", "0909123456", "123abc", "123abc"},
+				{"loilhpc01261", "loilh@gmail.com", "Lương Hữu Lợi", "9999", "123abc", "123abc"},
+				{"loilhpc01261", "loilh@gmail.com", "Lương Hữu Lợi", "0909123456", "123abc", "123"},
+		};
+	}
+}
